12478ec4ba73f811abc3c5647e22fe0fa46e7bfc
Abergaz <zagreba@gmail.com>
2019-08-06 15:20:57 +0300

    Конец OOP_FREE

---
+++ 1.OOP_Free/src/ru/javabegin/freeoop/training/fastjava2/OOP_15/Main.java
@@ -0,0 +1,12 @@
+package ru.javabegin.freeoop.training.fastjava2.OOP_15;
+
+import ru.javabegin.freeoop.training.fastjava2.OOP_15.objects.Phone;
+
+public class Main {
+    public static void main(String[] args) {
+        Phone phone = new Phone(1024, "Sony","GSM");
+        System.out.println(phone.getName()+" "+phone.getType());
+        phone.setName("iPhone");
+        System.out.println(phone.getName()+" "+phone.getType());
+    }
+}
