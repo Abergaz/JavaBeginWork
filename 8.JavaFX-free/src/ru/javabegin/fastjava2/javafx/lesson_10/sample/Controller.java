cb06c7a80061cdcb0809f51d09d2dc24533947bc
Abergaz <zagreba@gmail.com>
2019-11-07 11:19:54 +0300

    Основы JavaFX - 10. Обработка событий - lesson_10

---
+++ 8.JavaFX-free/src/ru/javabegin/fastjava2/javafx/lesson_10/sample/Controller.java
@@ -0,0 +1,15 @@
+package ru.javabegin.fastjava2.javafx.lesson_10.sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void firstButtonAction(ActionEvent actionEvent) {
+        System.out.println("button1");
+
+    }
+
+    public void secButtonAction(ActionEvent actionEvent) {
+        System.out.println("button2");
+    }
+}
