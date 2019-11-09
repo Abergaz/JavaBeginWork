8ee3a6e808d204eb48388893bf89be996dd794c8
Abergaz <zagreba@gmail.com>
2019-11-09 15:34:04 +0300

    Андресная книга на JavaFX - 6. Проектирование - lesson_06

---
+++ 9. JavaFX_n/src/ru/javabegin/fastjava2/javafx/lesson_6/objects/Person.java
@@ -0,0 +1,23 @@
+package ru.javabegin.fastjava2.javafx.lesson_6.objects;
+
+public class Person {
+
+    private String fio;
+    private String phone;
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
