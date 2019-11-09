5649502b2c501a117d70f533bd42de7f495207dd
Abergaz <zagreba@gmail.com>
2019-11-09 14:39:20 +0300

    Андресная книга на JavaFX - 4. Модальные окна - lesson_04

---
+++ 9. JavaFX_n/src/ru/javabegin/fastjava2/javafx/lesson_2/sample/Controller.java
@@ -0,0 +1,4 @@
+package ru.javabegin.fastjava2.javafx.lesson_2.sample;
+
+public class Controller {
+}
---
+++ 9. JavaFX_n/src/ru/javabegin/fastjava2/javafx/lesson_4/controllers/EditDialogController.java
@@ -0,0 +1,4 @@
+package ru.javabegin.fastjava2.javafx.lesson_4.controllers;
+
+public class EditDialogController {
+}
