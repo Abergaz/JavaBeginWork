d4bc8692df9f8d6739262b2214d4e0762fe30074
Abergaz <zagreba@gmail.com>
2019-11-10 10:45:59 +0300

    Андресная книга на JavaFX - 8. Заполнение TableView - lesson_08 и lesson_08h

---
+++ 9. JavaFX_n/src/ru/javabegin/fastjava2/javafx/lesson_8/controllers/EditDialogController.java
@@ -0,0 +1,24 @@
+package ru.javabegin.fastjava2.javafx.lesson_8.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class EditDialogController {
+
+    @FXML
+    private Button btnOk;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private TextField txtFIO;
+
+    @FXML
+    private TextField txtPhone;
+
+
+}
---
+++ 9. JavaFX_n/src/ru/javabegin/fastjava2/javafx/lesson_8h/controllers/EditDialogController.java
@@ -0,0 +1,25 @@
+package ru.javabegin.fastjava2.javafx.lesson_8h.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class EditDialogController {
+
+    @FXML
+    private Button btnOk;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private TextField txtFIO;
+
+    @FXML
+    private TextField txtPhone;
+
+
+
+}
