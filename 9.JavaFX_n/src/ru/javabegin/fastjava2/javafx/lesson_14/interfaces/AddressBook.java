8ea29cccd3c5440e129f7147ee1cb2a440271454
Abergaz <zagreba@gmail.com>
2019-11-10 16:06:59 +0300

    Андресная книга на JavaFX - 14. Переключение локали - lesson_14

---
+++ 9.JavaFX_n/src/ru/javabegin/fastjava2/javafx/lesson_14/interfaces/AddressBook.java
@@ -0,0 +1,17 @@
+package ru.javabegin.fastjava2.javafx.lesson_14.interfaces;
+
+
+import ru.javabegin.fastjava2.javafx.lesson_14.objects.Person;
+
+public interface AddressBook {
+
+    // добавить запись
+    void add(Person person);
+
+    // внести измененные значения (подтвердить измененные данные)
+    void update(Person person);
+
+    // удалить запись
+    void delete(Person person);
+
+}
---
+++ 9.JavaFX_n/src/ru/javabegin/fastjava2/javafx/lesson_14/utils/LocaleManager.java
@@ -0,0 +1,23 @@
+package ru.javabegin.fastjava2.javafx.lesson_14.utils;
+
+
+
+import ru.javabegin.fastjava2.javafx.lesson_14.objects.Lang;
+
+import java.util.Locale;
+
+public class LocaleManager {
+
+    public static final Locale RU_LOCALE = new Locale("ru");
+    public static final Locale EN_LOCALE = new Locale("en");
+
+    private static Lang currentLang;
+
+    public static Lang getCurrentLang() {
+        return currentLang;
+    }
+
+    public static void setCurrentLang(Lang currentLang) {
+        LocaleManager.currentLang = currentLang;
+    }
+}
