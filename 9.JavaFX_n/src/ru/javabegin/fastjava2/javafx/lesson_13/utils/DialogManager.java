bd6d16be4de2a7507a52c1d6a3146abd6f479ba0
Abergaz <zagreba@gmail.com>
2019-11-10 15:31:02 +0300

    Андресная книга на JavaFX - 13. Диалоговые окна - lesson_13

---
+++ 9.JavaFX_n/src/ru/javabegin/fastjava2/javafx/lesson_13/utils/DialogManager.java
@@ -0,0 +1,24 @@
+package ru.javabegin.fastjava2.javafx.lesson_13.utils;
+
+import javafx.scene.control.Alert;
+
+public class DialogManager {
+
+    public static void showInfoDialog(String title, String text){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setContentText(text);
+        alert.setHeaderText("");
+        alert.showAndWait();
+    }
+
+    public static void showErrorDialog(String title, String text){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(text);
+        alert.setHeaderText("");
+        alert.showAndWait();
+    }
+
+
+}
