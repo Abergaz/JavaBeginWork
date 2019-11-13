7e91b00db1923c0c57f578c8d42bdca22f06f39f
Abergaz <zagreba@gmail.com>
2019-11-13 17:01:00 +0300

    Андресная книга на JavaFX - 17. Запрос данных - lesson_17 и lesson_17h

---
+++ 9.JavaFX_n/src/ru/javabegin/fastjava2/javafx/lesson_17/interfaces/impls/DBAddressBook.java
@@ -0,0 +1,65 @@
+package ru.javabegin.fastjava2.javafx.lesson_17.interfaces.impls;
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import ru.javabegin.fastjava2.javafx.lesson_17.db.SQLiteConnection;
+import ru.javabegin.fastjava2.javafx.lesson_17.interfaces.AddressBook;
+import ru.javabegin.fastjava2.javafx.lesson_17.objects.Person;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBAddressBook implements AddressBook {
+
+    private ObservableList<Person> personList = FXCollections.observableArrayList();
+
+    @Override
+    public boolean add(Person person) {
+        throw new UnsupportedOperationException("Not implemented, yet");
+    }
+
+    @Override
+    public boolean delete(Person person) {
+        throw new UnsupportedOperationException("Not implemented, yet");
+    }
+
+    @Override
+    public ObservableList<Person> findAll() {
+
+        try (Connection con = SQLiteConnection.getConnection();
+             Statement statement = con.createStatement();
+             ResultSet rs = statement.executeQuery("select * from person");) {
+            while (rs.next()) {
+                Person person = new Person();
+                person.setId(rs.getInt("id"));
+                person.setFio(rs.getString("fio"));
+                person.setPhone(rs.getString("phone"));
+                personList.add(person);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBAddressBook.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return personList;
+    }
+
+    @Override
+    public boolean update(Person person) {
+        throw new UnsupportedOperationException("Not implemented, yet");
+    }
+
+    @Override
+    public ObservableList<Person> find(String text) {
+        throw new UnsupportedOperationException("Not implemented, yet");
+    }
+
+    public ObservableList<Person> getPersonList() {
+        return personList;
+    }
+}
---
+++ 9.JavaFX_n/src/ru/javabegin/fastjava2/javafx/lesson_17h/db/SQLiteConnection.java
@@ -0,0 +1,32 @@
+package ru.javabegin.fastjava2.javafx.lesson_17h.db;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class SQLiteConnection {
+
+
+
+    public static Connection getConnection() {
+        try {
+
+            Class.forName("org.sqlite.JDBC").newInstance();
+
+            // ���� � �� ���������� �������� � ��������� ���� ��������
+            String url = "jdbc:sqlite:D://JavaBeginWork//1.JavaBeginWork_Basic//9.JavaFX_n//src//ru//javabegin/fastjava2//javafx//lesson_17h//db//addressbook.db";// ��������� ������������� ���� � ����� ��
+
+            return DriverManager.getConnection(url);
+
+        } catch (SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+            Logger.getLogger(SQLiteConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+
+}
