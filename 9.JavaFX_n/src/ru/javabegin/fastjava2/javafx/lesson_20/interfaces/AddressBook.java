84fe6cb147934dc08f80d160a8abcfffe7d7e22e
Abergaz <zagreba@gmail.com>
2019-11-14 17:43:38 +0300

    Андресная книга на JavaFX - 20. Применение DAO - lesson_20

---
+++ 9.JavaFX_n/src/ru/javabegin/fastjava2/javafx/lesson_20/interfaces/AddressBook.java
@@ -0,0 +1,19 @@
+package ru.javabegin.fastjava2.javafx.lesson_20.interfaces;
+
+import javafx.collections.ObservableList;
+import ru.javabegin.fastjava2.javafx.lesson_20.objects.Person;
+
+
+public interface AddressBook {
+
+    boolean add(Person person);
+
+    boolean update(Person person);
+
+    boolean delete(Person person);
+
+    ObservableList<Person> findAll();
+
+    ObservableList<Person> find(String text);
+
+}
